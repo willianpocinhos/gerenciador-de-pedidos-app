19d40df15de16ea14cfde1569011932a8947d7ef
willianps88 <willianps88@outlook.com>
2024-03-26 13:57:47 -0300

    commit changes

--- front-end/src/services/index.js
+++ front-end/src/services/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiUrl = 'https://gerenciador-de-pedidos-app.vercel.app/'
+const apiUrl = 'https://gerenciador-de-pedidos-app.vercel.app'
 
 const address = axios.create({
     baseURL: apiUrl
